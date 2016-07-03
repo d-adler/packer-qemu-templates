63425176471cb4b908023d1d66fad11d89b50970
Daniel Adler <dadler1@gwdg.de>
2016-07-03 20:12:54 +0200

    added cloud-init, removed packages.sh from scripts in template

---
+++ debian/scripts/cloud-init.sh
@@ -0,0 +1,3 @@
+apt-get install -y cloud-init cloud-utils cloud-initramfs-growroot
+echo 'debian ALL=NOPASSWD:ALL' > /etc/sudoers.d/cloudinit
+# apt-get install -y cloud-init cloud-tools cloud-initiramfs-growroot
