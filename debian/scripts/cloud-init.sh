3674f86ae14c1845907b299c15a301307fe2515e
Daniel Adler <dadler1@gwdg.de>
2016-07-03 02:09:27 -0700

    fixes for debian

--- debian/scripts/cloud-init.sh
+++ debian/scripts/cloud-init.sh
@@ -1,3 +1,3 @@
 apt-get install -y cloud-init cloud-utils cloud-initramfs-growroot
-echo 'debian ALL=NOPASSWD:ALL' > /etc/sudoers.d/cloudinit
+echo 'debian ALL=NOPASSWD:ALL' > /etc/sudoers.d/cloud-init
 # apt-get install -y cloud-init cloud-tools cloud-initiramfs-growroot
